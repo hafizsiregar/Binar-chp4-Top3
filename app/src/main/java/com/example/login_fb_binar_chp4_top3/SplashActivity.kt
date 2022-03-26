803ffdd07ea4783efdaa88a2f2226c345f9e4160
hafizsiregar <hafizmessi81@gmail.com>
2022-03-26 08:58:08 +0700

    UI login fb

---
+++ app/src/main/java/com/example/login_fb_binar_chp4_top3/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.example.login_fb_binar_chp4_top3
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+        },3000)
+    }
+}
