45f51521b110209a4ecd140488963f6b6f5c1b5b
hafizsiregar <hafizmessi81@gmail.com>
2022-03-28 10:17:08 +0700

    edit configure at manifest

--- app/src/main/java/com/example/login_fb_binar_chp4_top3/SplashActivity.kt
+++ app/src/main/java/com/example/login_fb_binar_chp4_top3/SplashActivity.kt
@@ -9,6 +9,7 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        supportActionBar?.hide()
 
         Handler().postDelayed({
             startActivity(Intent(this, LoginActivity::class.java))
